478aef5eb3c7902ad38a5496b0fa86d64872551b
Shepelkov Artur <shepelkovartur@gmail.com>
2022-03-21 18:35:08 +0300

    Добавьте файлы проекта.

---
+++ Test/Program.cs
@@ -0,0 +1,30 @@
+﻿//проверка входных данных
+if (int.TryParse(Console.ReadLine(), out int number)) { }
+else Console.WriteLine("В следующий раз введите число:)");
+
+#region Половинка елки
+string tree = "";
+for (int i = 0; i < number; i++)
+{
+    tree += "*";
+    Console.WriteLine(tree);
+}
+#endregion
+
+#region Целиковая елка
+int decrementNumber = number;
+string secondTree = "*";
+string secondTreeHelper = "";
+
+for (int i = 0; i < number; i++)
+{
+    for (int j = 0; j < decrementNumber; j++)
+    {
+        secondTreeHelper += " ";
+    }
+    Console.WriteLine(secondTreeHelper + secondTree);
+    secondTree += "**";
+    decrementNumber--;
+    secondTreeHelper = "";
+}
+#endregion
